df8403f913a36cfa0a1b92b76969d816174ac954
Laura Krey <80431530+lkrey@users.noreply.github.com>
2021-09-02 19:09:37 -0400

    [New Feature]: start- Pacman

---
+++ js-pacman-START-HERE/Pacman.js
@@ -0,0 +1,45 @@
+import { object } from "webidl-conversions";
+import { OBJECT_TYPE, DIRECTIONS } from "./setup";
+
+class Pacman {
+  constructor(speed, startPos) {
+    this.pos = startPos;
+    this.speed = speed;
+    this.dir = null;
+    this.timer = 0;
+    this.powerPill = false;
+    this.rotation = true;
+  }
+
+  shouldMove() {
+    if(!this.direction) return false;
+
+    if(this.timer === this.speed) {
+      this.timer = 0;
+      return true;
+    }
+
+    this.timer++;
+  }
+
+  getNextMove(objectExist) {
+    let nextMovePos = this.pos + this.dir.movement;
+
+    if (objectExist(nextMovePos, OBJECT_TYPE.WALL) ||
+       object(nextMovePos, OBJECT_TYPE.GHOSTLAIR)) {
+        nextMovePos = this.pos;
+      }
+
+      return {
+        nextMovePos, direction: this.dir
+      }
+  }
+
+  makeMove() {
+    const classesToRemove = [OBJECT_TYPE.PACMAN];
+    const classesToAdd = [OBJECT_TYPE.PACMAN];
+
+    return {classesToRemove, classesToAdd}
+  }
+
+}
