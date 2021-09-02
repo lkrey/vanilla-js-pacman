4dd6e7e31e32c8c4d666f4ef2755ecc33a240928
Laura Krey <80431530+lkrey@users.noreply.github.com>
2021-09-02 18:43:33 -0400

    [Framkework]: basic game functions

--- js-pacman-START-HERE/index.js
+++ js-pacman-START-HERE/index.js
@@ -1,1 +1,33 @@
-console.log('Working!');
+import {LEVEL, OBJECT_TYPE} from "./setup";
+
+// DOM elements
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
+
+// constraints
+const POWER_PILL_TIME = 10000;
+const GLOBAL_SPEED = 80; //ms
+
+// setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {
+
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts) {
+
+}
+
+function startGame() {
+
+}
